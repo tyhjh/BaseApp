068c095edc865541e49b546e3936ec2f00ef6c7b
Tyhj <1043315346@qq.com>
2018-04-05 14:17:35 +0800

    优化RecycleView

--- tyhjlibrary/src/main/java/com/yorhp/tyhjlibrary/view/recycleView/MRecycleView.java
+++ tyhjlibrary/src/main/java/com/yorhp/tyhjlibrary/view/recycleView/MRecycleView.java
@@ -2,8 +2,10 @@ package com.yorhp.tyhjlibrary.view.recycleView;
 
 import android.content.Context;
 import android.support.annotation.Nullable;
+import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.StaggeredGridLayoutManager;
 import android.util.AttributeSet;
 
 /**
@@ -39,6 +41,7 @@ public class MRecycleView extends RecyclerView {
     }
 
 
+    //添加滑动到底部的监听
     public void initRecycleView() {
         this.addOnScrollListener(new RecyclerView.OnScrollListener() {
             @Override
@@ -48,12 +51,24 @@ public class MRecycleView extends RecyclerView {
                 //全部item的数量
                 int totalItemCount = recyclerView.getAdapter().getItemCount();
                 //最后一个可见item的position
-                int lastVisibleItemPosition = lm.findLastVisibleItemPosition();
+                int lastVisibleItemPosition;
                 //可见item的数量
                 int visibleItemCount = recyclerView.getChildCount();
+                LayoutManager layoutManager = getLayoutManager();
+
+                if (layoutManager instanceof GridLayoutManager) {
+                    lastVisibleItemPosition = ((GridLayoutManager) layoutManager).findLastVisibleItemPosition();
+                } else if (layoutManager instanceof StaggeredGridLayoutManager) {
+                    int[] into = new int[((StaggeredGridLayoutManager) layoutManager).getSpanCount()];
+                    ((StaggeredGridLayoutManager) layoutManager).findLastVisibleItemPositions(into);
+                    lastVisibleItemPosition = findMax(into);
+                } else {
+                    lastVisibleItemPosition = ((LinearLayoutManager) layoutManager).findLastVisibleItemPosition();
+                }
+
                 if (newState == RecyclerView.SCROLL_STATE_IDLE
                         && lastVisibleItemPosition == totalItemCount - 1
-                        && visibleItemCount > 0) {
+                        && totalItemCount>5&&totalItemCount>visibleItemCount) {
                     if (recycleViewEventListener != null) {
                         recycleViewEventListener.onScollToBottom();
                     }
@@ -82,6 +97,18 @@ public class MRecycleView extends RecyclerView {
     public interface RecycleViewEventListener {
         void onScollToBottom();
     }
+
+
+    private int findMax(int[] lastPositions) {
+        int max = lastPositions[0];
+        for (int value : lastPositions) {
+            if (value > max) {
+                max = value;
+            }
+        }
+        return max;
+    }
+
 
 
 }
